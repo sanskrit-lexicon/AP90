cd7f78f2a784ba65c537e787990735f4148f5626
funderburkjim <funderburkjim@gmail.com>
2020-07-07 16:48:35 -0400

    Use sanskrit-lexicon/MWS/mwverbs/mwverbs1.txt

--- verbs01/redo.sh
+++ verbs01/redo.sh
@@ -1,13 +1,14 @@
-echo "remake mwverbs"
+#echo "remake mwverbs"
 orig="../../../cologne/csl-orig/v02"
+mwverbs1="../../MWS/mwverbs/mwverbs1.txt"
 #python mwverb.py mw ../../mw/mw.txt mwverbs.txt
-python mwverb.py mw ${orig}/mw/mw.txt mwverbs.txt
-echo "remake mwverbs1"
-python mwverbs1.py mwverbs.txt mwverbs1.txt
+#python mwverb.py mw ${orig}/mw/mw.txt mwverbs.txt
+#echo "remake mwverbs1"
+#python mwverbs1.py mwverbs.txt mwverbs1.txt
 echo "remake ap90_verb_filter.txt"
 python ap90_verb_filter.py ${orig}/ap90/ap90.txt ap90_verb_exclude.txt ap90_verb_include.txt ap90_verb_filter.txt
 echo "remake ap90_verb_filter_map.txt"
-python ap90_verb_filter_map.py ap90_verb_filter.txt  mwverbs1.txt ap90_verb_filter_map.txt
+python ap90_verb_filter_map.py ap90_verb_filter.txt  ${mwverbs1} ap90_verb_filter_map.txt
 
 echo "ap90_verb1.txt"
 python verb1.py slp1 ${orig}/ap90/ap90.txt ap90_verb_filter_map.txt ap90_verb1.txt
